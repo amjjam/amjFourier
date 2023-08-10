572d4b43233c3df8bf6ec158f4c6cde7e704db0b
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2023-08-10 16:00:08 -0600

    Moved template code to amjForier.H

--- src/Frame.C
+++ src/Frame.C
@@ -1,43 +1,12 @@
-#include "../include/Frame.H"
+#include "../include/amjFourier.H"
 
-template<class T>
-Frame::Frame(int nL, int nF):_nL(nL),_nF(nF){
-  _frame.resize(nL);
-  for(int iL=0;iL<nL;iL++)
-    _frame[iL].resize(nF);
-}
 
-template<class T>
-Frame<T> &Frame::operator*=(double f){
-  for(int iL=0;iL<nL;iL++)
-    for(int iF=0;iF<nF;iF++)
-      _frame[iL][iF]*=f;
-  return *this;
-}
-
-template<class T>
-Frame<T> &Frame::operator+=(const Frame<T> &f){
-  for(int iL=0;iL<nL;iL++)
-    for(int iF=0;iF<nF;iF++)
-      _frame[iL][iF]+=f[iL][iF];
-  return *this;
-}
-
-template<class T>
-Frame<T> &Frame::operator=(const Frame<T> &f){
-  _nL=f._nL;
-  _nF=f._nF;
-  _frame.resize(_nL);
-  for(int iL=0;i<nL;iL++)
-    _frame[iL]=f._frame[iL];
-}
-
-Frame<uint16_t> poisson(const Frame<couble> &in, long &seed){
+Frame<uint16_t> poisson(const Frame<double> &in, long &seed){
   Frame<uint16_t> out(in.nL(),in.nF());
 
   for(unsigned int iL=0;iL<in.nL();iL++)
     for(unsigned int iF=0;iF<in.nF();iF++)
-      out[iL][iF]=poidev(int[iL][iF],&seed);
+      out[iL][iF]=poidev(in[iL][iF],&seed);
   
   return out;
 }
